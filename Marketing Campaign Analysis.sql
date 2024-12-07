a4b646bb184174061c4ba18faa7ec9ad335ffea4
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2024-12-07 12:08:09 +0100

    Update Marketing Campaign Analysis.sql

--- Marketing Campaign Analysis.sql
+++ Marketing Campaign Analysis.sql
@@ -1,20 +1,19 @@
 USE MeriskillOrderData;
 
--- Data Cleaning
 
 SELECT * FROM dbo.Order_Data_meriskill;
 
--- 1. Remove Duplicates
--- 2. Standardize the data
--- 3. Null values or blank values
--- 4. Remove columns and row that are not relevant
 
+-- DATA CLEANING PROCESS
 
-    SELECT *,
-           ROW_NUMBER() OVER (PARTITION BY OrderID, TransactionID ORDER BY OrderID) AS RowNum
-    FROM dbo.Order_Data_meriskill;
+-- STEP 1: REMOVE DUPLICATES
 
+SELECT OrderID, CustID, ProductSKU, Customer_Name, count(*)
+FROM dbo.Order_Data_meriskill
+GROUP BY OrderID, CustID, ProductSKU, Customer_Name;
 
+   
+-- STEP 2: TRIM WHITESPACES
 UPDATE dbo.Order_Data_meriskill
 SET Customer_Name = LTRIM(RTRIM(Customer_Name)),
     AcquisitionSource = LTRIM(RTRIM(AcquisitionSource));
